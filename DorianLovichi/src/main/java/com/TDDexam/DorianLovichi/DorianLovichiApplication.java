7ebea5d50ae5c0ed414410bcfde7a5f773681034
Dorian Lovichi <dorianppl852@gmail.com>
2025-04-17 09:22:08 +0200

    ini

---
+++ DorianLovichi/src/main/java/com/TDDexam/DorianLovichi/DorianLovichiApplication.java
@@ -0,0 +1,13 @@
+package com.TDDexam.DorianLovichi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DorianLovichiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DorianLovichiApplication.class, args);
+	}
+
+}
---
+++ DorianLovichi/src/test/java/com/TDDexam/DorianLovichi/DorianLovichiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TDDexam.DorianLovichi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DorianLovichiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
